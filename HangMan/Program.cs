b0d3ee965dfc4313f687e7ee1c7c7453b5db59f1
Elias <elias_sixtos@outlook.com>
2021-09-06 21:36:39 -0400

    Created HiddenWord list and checked to see if playerGuess was correct to replace it to all hiddenList slots.

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HangMan
 {
@@ -6,7 +8,49 @@ namespace HangMan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> listWords = new List<string> {
+            "jacked", "append", "ripped", "lambda", "Caucus", "Hugged",
+            "Skinny", "Object", "Squint", "Speedy", "Ascend"
+            };
+
+
+            Random rng = new Random();
+            string randWord = listWords[rng.Next(listWords.Count())];
+
+            List<char> answerCharList = new List<char>();
+            List<string> hiddenWord = new List<string>();
+
+            foreach (char item in randWord)
+            {
+                answerCharList.Add(item);
+                hiddenWord.Add("_");
+            }
+
+            int attempts = 0;
+
+            while (attempts < randWord.Length + 3)
+            {
+                Console.WriteLine(string.Join(" ", hiddenWord));
+                Console.WriteLine("Guess a letter!");
+                char playerGuess = Convert.ToChar(Console.ReadLine());
+
+                if (answerCharList.Contains(playerGuess))
+                {
+                    for (int i = 0; i < answerCharList.Count; i++)
+                    {
+                        if (answerCharList[i] == playerGuess)
+                        {
+                            hiddenWord[i] = Convert.ToString(playerGuess);
+                        }
+                    }
+                }
+
+
+
+
+            }
+
+
         }
     }
 }
